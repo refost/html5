1956e1d6ecceabec1d563c6cd54f96dc2a827da4
refost <refost220@gmail.com>
2021-06-10 19:09:17 +0300

    Fourth commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    slidesToShow: 2,
+    arrows: false,
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 500,
+    draggable: true,
+    swipe: false,
+  });
+})
